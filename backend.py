64e8f428bff3606e75d98b0ddb25b30c7f54efd0
oktavianuu <oktavianuss.misro@gmail.com>
2024-05-24 11:12:26 +0700

    update backend.py

--- backend.py
+++ backend.py
@@ -58,6 +58,10 @@ def predict():
 def index():
     return send_from_directory('', 'index.html')
 
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
